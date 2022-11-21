45589f9edc6912de06bfd08ea18e3fa4e7e3288b
rittikbasu <irittik@gmail.com>
2022-11-21 22:48:57 +0530

    feat: replace svg with icons + break into comp

--- components/Table.js
+++ components/Table.js
@@ -1,19 +1,22 @@
 import { useState, Fragment } from "react";
 import Link from "next/link";
 import Moment from "react-moment";
+import clsx from "clsx";
+import { HiChevronDown, HiSearch } from "react-icons/hi";
+import { FiExternalLink } from "react-icons/fi";
 
 export default function Table(data) {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-
-  const [userdata, setUserdata] = useState(data.data);
-  const itemCount = userdata.length;
-  const currentItems = userdata.slice(
+  const [jobData, setJobData] = useState(data.data);
+  const itemCount = jobData.length;
+  const currentItems = jobData.slice(
     indexOfFirstItem,
     indexOfLastItem
   );
+  const fields = ["posted by", "title", "time", "batch", "link"];
 
   // change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
@@ -43,37 +46,38 @@ export default function Table(data) {
         return item.title.toLowerCase().includes(keyword);
       });
       setCurrentPage(1);
-      setUserdata(filteredData);
+      setJobData(filteredData);
     }, 200);
   }
 
-  function getYC(string) {
-    const regex = /\((YC\s\S+)\)/;
+  // regex to get YC something from text
+  // const regex = /YC\s\S+/g;
+
+  function getBatch(string) {
+    const regex = /YC\s\S+/g;
     const match = string.match(regex);
     if (match) {
       return match[0]
-        .replace("(", "")
         .replace(")", "")
-        .replace("YC", "");
+        .replace("YC", "")
+        .replace(",", "");
     } else {
       return "";
     }
   }
-  function getTitle(string) {
+  function removeBatchFromTitle(string) {
     return string.replace(/\(YC\s\S+/, "");
   }
   return (
     <div className="container mx-auto px-4 sm:px-8">
       <div className="py-4">
-        <div className="flex justify-between items-center">
-          <h2 className="text-2xl md:text-3xl font-semibold leading-tight text-orange-400  tracking-widest md:tracking-wider">
-            YC <span className="text-zinc-300">Job Client</span>
+        <div className="flex justify-center md:justify-between items-center">
+          <h2 className="text-3xl font-semibold leading-tight text-orange-400  tracking-widest md:tracking-wider">
+            YC <span className="text-zinc-300">Job Board</span>
           </h2>
-          <div className="rounded-xl bg-gradient-to-r p-1 from-orange-400 via-red-400 to-orange-300 hidden md:flex">
-            <h2 className="text-xl bg-white p-2 rounded-lg font-semibold leading-tight text-zinc-400/80 tracking-widest md:tracking-wider">
-              Making your job search easier
-            </h2>
-          </div>
+          <h2 className="text-3xl bg-white p-2 rounded-lg font-semibold leading-tight animate-gradient bg-gradient-to-r from-amber-300 via-orange-500 to-amber-300 bg-clip-text text-transparent tracking-widest md:tracking-wider hidden md:flex">
+            Making your job search easier
+          </h2>
         </div>
         <div className="mt-12 mb-4 md:mt-16 md:mb-8 flex flex-row md:justify-between justify-center">
           <div className="flex flex-row mb-1 sm:mb-0">
@@ -87,24 +91,13 @@ export default function Table(data) {
                 <option value="50">Show 50</option>
               </select>
               <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
+                <HiChevronDown />
               </div>
             </div>
           </div>
           <div className="block relative">
             <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-              <svg
-                viewBox="0 0 24 24"
-                className="h-4 w-4 fill-current text-gray-500"
-              >
-                <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
-              </svg>
+              <HiSearch className="stroke-1 stroke-slate-300" />
             </span>
             <input
               placeholder="Search"
@@ -116,92 +109,58 @@ export default function Table(data) {
         <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
           <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
             <table className="min-w-full leading-normal">
-              <thead>
-                <tr>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-orange-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider hidden md:table-cell">
-                    Posted By
-                  </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-orange-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Title
-                  </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-orange-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Time
-                  </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-orange-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider hidden md:table-cell">
-                    YC
-                  </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-orange-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider hidden md:table-cell">
-                    Link
-                  </th>
-                </tr>
-              </thead>
+              <TableHead fields={fields} />
               <tbody>
                 {currentItems.length !== 0 &&
-                  currentItems.map((user) => (
-                    <tr key={user.id}>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm hidden md:table-cell text-center underline">
+                  currentItems.map((job) => (
+                    <tr key={job.id}>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm hidden md:table-cell text-center underline md:hover:decoration-orange-500">
                         <Link
-                          href={`https://news.ycombinator.com/user?id=${user.by}`}
+                          href={`https://news.ycombinator.com/user?id=${job.by}`}
                           className="text-gray-900 whitespace-no-wrap"
                         >
-                          {user.by}
+                          {job.by}
                         </Link>
                       </td>
                       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm md:text-center md:table-cell hidden">
                         <p className="text-gray-900 whitespace-no-wrap">
-                          {getTitle(user.title)}
+                          {removeBatchFromTitle(job.title)}
                         </p>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm md:text-center decoration-orange-500 md:hidden">
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm md:text-center decoration-blue-500 md:hidden">
                         <Link
-                          href={user.url}
+                          href={job.url}
                           className="text-gray-900 whitespace-no-wrap underline"
                         >
-                          {getTitle(user.title)}
+                          {removeBatchFromTitle(job.title)}
                         </Link>
                       </td>
                       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-center">
                         <p className="text-gray-900 whitespace-no-wrap">
                           <Moment fromNow unix>
-                            {user.time}
+                            {job.time}
                           </Moment>
                         </p>
                       </td>
                       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm  hidden md:table-cell text-center">
-                        {getYC(user.title) && (
+                        {getBatch(job.title) && (
                           <span className="relative inline-block px-3 py-1 font-semibold text-orange-600 leading-tight whitespace-no-wrap">
                             <span
                               aria-hidden
                               className="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
                             ></span>
                             <span className="relative">
-                              {getYC(user.title)}
+                              {getBatch(job.title)}
                             </span>
                           </span>
                         )}
                       </td>
                       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm hidden md:table-cell text-center">
                         <Link
-                          href={user.url}
-                          className="text-gray-900 flex justify-center md:hover:text-orange-600"
+                          href={job.url}
+                          className="text-gray-900 flex justify-center"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            className="bi bi-box-arrow-up-right"
-                            viewBox="0 0 16 16"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
-                            />
-                            <path
-                              fillRule="evenodd"
-                              d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
-                            />
-                          </svg>
+                          <FiExternalLink className="stroke-2 stroke-slate-500 h-5 w-5 md:hover:stroke-orange-500" />
                         </Link>
                       </td>
                     </tr>
@@ -267,5 +226,28 @@ function Navigation({
         </div>
       )}
     </Fragment>
+  );
+}
+
+function TableHead({ fields }) {
+  const className =
+    "px-5 py-3 border-b-2 border-gray-200 bg-orange-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider";
+  return (
+    <thead>
+      <tr>
+        {fields.map((field, index) => (
+          <th
+            key={field}
+            className={
+              field === "title" || field === "time"
+                ? className
+                : clsx(className, "hidden md:table-cell")
+            }
+          >
+            {field}
+          </th>
+        ))}
+      </tr>
+    </thead>
   );
 }
